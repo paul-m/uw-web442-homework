11b3f05f39541d7e0db71c7a981ccf3da734373a
Paul Mitchum <paul@mile23.com>
2013-02-04 15:09:39 -0800

    lab4 done

--- lab4/index.php
+++ lab4/index.php
@@ -9,6 +9,9 @@ namespace Lab4;
 // Include the database array from settings.php.
 include 'settings.php';
 
+// Global storage for our PDO object.
+$pdo = NULL;
+
 function echo_div($msg = '') {
   echo '<div>' . $msg . '</div>';
 }
@@ -41,6 +44,7 @@ function create_table($pdo) {
   
   if ($tableExists) return TRUE;
 
+  // Doesn't exist yet so we'll create it.
   $query = 'CREATE TABLE `User` ( `id` int(11) NOT NULL AUTO_INCREMENT, `firstname` varchar(255) DEFAULT NULL, `lastname` varchar(255) DEFAULT NULL, PRIMARY KEY (`id`) ) ENGINE=InnoDB';
   $tableExists = $pdo->query($query);
 
@@ -48,8 +52,58 @@ function create_table($pdo) {
   return FALSE;
 }
 
-// Global storage for our PDO object.
-$pdo = NULL;
+function insert_row($pdo, $firstname = '', $lastname = '') {
+  // Set up our query.
+  $query = $pdo->prepare("INSERT INTO User (`firstname`, `lastname`) VALUES (:first, :last)");
+
+  // Bind the paramaters
+  $query->bindParam(':first', $firstname, \PDO::PARAM_STR, 255);
+  $query->bindParam(':last', $lastname, \PDO::PARAM_STR, 255);
+  
+  $query->execute();
+}
+
+function get_last_row($pdo) {
+  $query = $pdo->prepare("SELECT * FROM User WHERE `id` = (SELECT MAX(id) FROM User)");
+  $query->execute();
+  return $query->fetchAll(\PDO::FETCH_ASSOC);
+}
+
+function all_records($pdo) {
+  $query = $pdo->prepare("SELECT * FROM User");
+  $query->execute();
+  return $query->fetchAll(\PDO::FETCH_ASSOC);
+}
+
+function table_array($title = '', $data = array()) {
+  $keys = array('id', 'firstname', 'lastname');
+  if (count($data) < 1) {
+    echo '<div>No tabular data to display.</div>';
+    return;
+  }
+  // Set up the table wrapper elements.
+  echo '<table border="1"><caption>' . $title . '</caption>';
+  echo '<tr>';
+  foreach ($keys as $key) {
+    echo '<th>' . $key . '</th>';
+  }
+  echo '</tr>';
+  // Show the data itself.
+  foreach($data as $datum) {
+    echo '<tr>';
+    foreach($keys as $key) {
+      echo '<td>' . $datum[$key] . '</td>';
+    }
+    echo '</tr>';
+  }
+  // Close up the table.
+  echo '</table>';
+}
+
+
+// main() {
+
+// Try to connect to the DB.
 try {
   $pdo = connect_db($database);
 } catch (\Exception $e) {
@@ -57,14 +111,18 @@ try {
   exit(1);
 }
 
+// Try to create the table.
 if (create_table($pdo)) {
   // Insert a new row into the table
-  echo_div('created table or it existed already.');
-  $insert_these = array();
-/*  for ($i=0; $i < 10; ++$i) {
-    
-  }*/
+  insert_row($pdo, 'Jay', 'Zeng');
+  insert_row($pdo, 'Paul', 'Mitchum');
+  // Get last row inserted
+  $last_insert = get_last_row($pdo);
+  table_array('Last inserted record', $last_insert);
+  // Get all records
+  $all_records = all_records($pdo);
+  table_array('All records', $all_records);
 }
 else {
-  echo_div('unable to create table in database.');
+  echo_div('unable to find or create table in database.');
 }
